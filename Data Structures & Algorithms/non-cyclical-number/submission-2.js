be9d76141956537c9337a3ef984f4354236b0c5a
Jonathan Edward Dressel <55971295+JonathanEDressel@users.noreply.github.com>
2026-04-21 13:31:52 -0700

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {boolean}
+     */
+    getDigit(n) {
+        var res = 0;
+        while(n > 0) {
+            var d = n % 10;
+            res += (d * d);
+            n = Math.floor(n / 10);
+        }
+        return res;
+    }
+
+    isHappy(n) {
+        var tracker = new Map();
+        while(n !== 1) {
+            n = this.getDigit(n);
+            if(tracker.has(n))
+                return false;
+            tracker.set(n, 1);
+        }
+        return true;
+    }
+}
