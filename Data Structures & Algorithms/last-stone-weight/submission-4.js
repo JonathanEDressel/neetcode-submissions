813f3ddb8d819f7d919e7ebb624593ba3bd301fd
Jonathan Edward Dressel <55971295+JonathanEDressel@users.noreply.github.com>
2026-04-21 12:08:25 -0700

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeight(stones) {
+        stones.sort((x, y) => x - y);
+        
+        while(stones.length > 1) {
+            var d1 = stones.pop();
+            var d2 = stones.pop();
+            if (d1 !== d2) {
+                var d = d1 > d2 ? (d1 - d2) : (d2 - d1);
+                stones.push(d);
+            }
+            stones.sort((x, y) => x - y);
+        }
+        return stones.length > 0 ? stones[0] : 0;
+    }
+}
