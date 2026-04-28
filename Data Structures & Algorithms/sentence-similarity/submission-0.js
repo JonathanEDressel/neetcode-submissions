59531f0718c2a5ae84f93864cc12979071e0358d
Jonathan Edward Dressel <55971295+JonathanEDressel@users.noreply.github.com>
2026-04-28 08:31:24 -0700

    Add: sentence-similarity - submission-0

---
+++ Data Structures & Algorithms/sentence-similarity/submission-0.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} sentence1
+     * @param {string[]} sentence2
+     * @param {string[][]} similarPairs
+     * @return {boolean}
+     */
+    areSentencesSimilar(sentence1, sentence2, similarPairs) {
+        if(sentence1.length !== sentence2.length)
+            return false;
+        var tracker = new Map();
+
+        for(var i = 0; i < sentence1.length; i++) {
+            tracker.set(sentence1[i], sentence2[i]);
+        }
+
+        for(var i = 0; i < sentence1.length; i++) {
+            if(sentence1[i] === sentence2[i])
+                continue;
+            if(tracker.get(sentence1[i]) === sentence2[i] || (tracker.get(sentence2[i]) === sentence1[i]))
+                continue;
+            return false;
+        }
+        return true;
+    }
+}
