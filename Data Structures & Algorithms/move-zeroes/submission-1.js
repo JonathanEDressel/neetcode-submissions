9a71a87df70e08431ef89744ce4674764d7cba8d
Jonathan Edward Dressel <55971295+JonathanEDressel@users.noreply.github.com>
2026-05-02 09:24:04 -0700

    Add: move-zeroes - submission-1

---
+++ Data Structures & Algorithms/move-zeroes/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    moveZeroes(nums) {
+        
+
+        //[1, 0, 2, 0, 0, 3, 4]
+        // r
+        // l
+        var track = [];
+        for(var i = 0; i < nums.length; i++) {
+            if(nums[i] !== 0)
+                track.push(nums[i]);
+        }
+        var count = track.length;
+        var idx = 0;
+        for(var i = 0; i < nums.length; i++) {
+            if(idx < count) {
+                nums[i] = track[idx];
+                idx++;
+            }
+            else
+                nums[i] = 0;
+        }
+
+        return nums;
+    }
+}
