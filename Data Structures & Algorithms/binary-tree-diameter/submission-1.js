bb24bc3c9bb7da456956359a1cfc02947dd29e55
Jonathan Edward Dressel <55971295+JonathanEDressel@users.noreply.github.com>
2026-05-04 11:46:30 -0700

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    findMax(root, res) {
+        if(!root)
+            return 0;
+        
+        var left = this.findMax(root.left, res);
+        var right = this.findMax(root.right, res);
+        res[0] = Math.max(res[0], left + right);
+        return 1 + Math.max(left, right);
+    }
+
+    diameterOfBinaryTree(root) {
+        var res = [0];
+        this.findMax(root, res);
+        return res[0];
+    }
+}
