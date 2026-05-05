97ad5ee009235713d6b9bfc914b220cb29f967e2
Jonathan Edward Dressel <55971295+JonathanEDressel@users.noreply.github.com>
2026-05-05 11:24:35 -0700

    Add: subtree-of-a-binary-tree - submission-8

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-8.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} subRoot
+     * @return {boolean}
+     */
+
+    
+
+    isSame(root, sub) {
+        if(!root && !sub)
+            return true;
+        if(!root || !sub || root.val !== sub.val)
+            return false;
+            
+        return this.isSame(root.left, sub.left) && this.isSame(root.right, sub.right);
+    }
+
+    isSubtree(root, subRoot) {
+        if(!subRoot)
+            return true;
+        if(!root)
+            return false;
+        
+        if(this.isSame(root, subRoot))
+            return true;
+        return this.isSubtree(root.left, subRoot) || this.isSubtree(root.right, subRoot);
+    }
+}
