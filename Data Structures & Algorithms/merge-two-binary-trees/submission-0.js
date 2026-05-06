56bffc137f80dfbefd7f5a14a52665be228c4eec
Jonathan Edward Dressel <55971295+JonathanEDressel@users.noreply.github.com>
2026-05-06 09:42:51 -0700

    Add: merge-two-binary-trees - submission-0

---
+++ Data Structures & Algorithms/merge-two-binary-trees/submission-0.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root1
+     * @param {TreeNode} root2
+     * @return {TreeNode}
+     */
+
+    createTree(root1, root2) {
+        if(!root1 && !root2)
+            return null;
+
+        var left;
+        if(root1 && root2)
+            left = this.createTree(root1.left, root2.left);
+        else if (!root1 && root2)
+            left = this.createTree(null, root2.left);
+        else
+            left = this.createTree(root1.left, null);
+
+        var right;
+        if(root1 && root2)
+            right = this.createTree(root1.right, root2.right);
+        else if (!root1 && root2)
+            right = this.createTree(null, root2.right);
+        else
+            right = this.createTree(root1.left, null);
+
+
+        var val = (root1?.val ?? 0) + (root2?.val ?? 0);
+        var node = new TreeNode(val);
+        node.left = left;
+        node.right = right;
+        return node;
+    }
+
+    mergeTrees(root1, root2) {
+        return this.createTree(root1, root2);;
+    }
+}
