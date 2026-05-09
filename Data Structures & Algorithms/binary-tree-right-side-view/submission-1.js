b4c45ddf8d6f32b64c02878d25d74b997ab84031
Jonathan Edward Dressel <55971295+JonathanEDressel@users.noreply.github.com>
2026-05-09 16:36:15 -0700

    Add: binary-tree-right-side-view - submission-1

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-1.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    rightSideView(root) {
+        if(!root)
+            return [];
+        var res = [];
+
+        var qu = [];
+        qu.push(root);
+        while(qu.length > 0) {
+            var len = qu.length;
+            for(var i = len - 1; i >= 0; i--) {
+                var curr = qu.shift();
+                if(i === (len - 1))
+                    res.push(curr.val);
+
+                if(curr.right)   
+                    qu.push(curr.right);
+                if(curr.left)
+                    qu.push(curr.left);
+            }
+        }
+
+        return res;
+    }
+}
