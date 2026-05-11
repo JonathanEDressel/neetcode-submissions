51ba819729fb7eea627aaa2318dad0aa594de0cf
Jonathan Edward Dressel <55971295+JonathanEDressel@users.noreply.github.com>
2026-05-11 10:59:32 -0700

    Add: path-sum - submission-6

---
+++ Data Structures & Algorithms/path-sum/submission-6.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} targetSum
+     * @return {boolean}
+     */
+
+    dfs(root, targetSum) {
+        if(!root)
+            return false;
+        targetSum -= root.val;
+        if (!root.left && !root.right)
+            return targetSum === 0;
+        if(this.dfs(root.right, targetSum) || this.dfs(root.left, targetSum))
+            return true;
+        return false;
+    }
+
+    hasPathSum(root, targetSum) {
+        return this.dfs(root, targetSum);
+    }
+}
