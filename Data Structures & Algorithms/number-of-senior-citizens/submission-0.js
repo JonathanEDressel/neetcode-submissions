bd1246ea4ebcfc43a0fc291a54ba42233ed03e74
Jonathan Edward Dressel <55971295+JonathanEDressel@users.noreply.github.com>
2026-05-14 20:45:46 -0700

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} details
+     * @return {number}
+     */
+
+    isGreaterThan60(str) {
+        for(var i = 0; i < str.length; i++) {
+            if(str.charCodeAt(i) >= 48 && str.charCodeAt(i) <= 57)
+                return parseInt(str[i] + str[i+1]) > 60;
+        }
+    }
+
+    countSeniors(details) {
+        var res = 0;
+        for(var i = 0; i < details.length; i++)
+            if(this.isGreaterThan60(details[i]))
+                res++;
+        return res;
+    }
+}
