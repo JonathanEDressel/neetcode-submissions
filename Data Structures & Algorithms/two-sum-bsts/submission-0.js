31d50a60d33d9e39648156973ba75982edd6a0b0
Jonathan Edward Dressel <55971295+JonathanEDressel@users.noreply.github.com>
2026-05-17 14:54:05 -0700

    Add: two-sum-bsts - submission-0

---
+++ Data Structures & Algorithms/two-sum-bsts/submission-0.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root1
+     * @param {TreeNode} root2
+     * @param {number} target
+     * @return {boolean}
+     */
+
+    dfs(root, arr) {
+        if(!root)
+            return;
+        arr.push(root.val);
+        this.dfs(root.left, arr);
+        this.dfs(root.right, arr);
+    }
+
+    twoSumBSTs(root1, root2, target) {
+        var arr1 = [], arr2 = [];
+        this.dfs(root1, arr1);
+        this.dfs(root2, arr2);
+        var track = new Map(); 
+
+        for(var i = 0; i < arr1.length; i++) {
+            var d = Math.abs(arr1[i] - target);
+            track.set(d, arr1[i]);
+        }
+
+        for(var i = 0; i < arr2.length; i++) {
+            if(track.has(arr2[i]))
+                return true;
+        }
+        return false;
+    }
+}
